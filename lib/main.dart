968f64759a4510f6d98fa4c2b8d2334dd5748d31
AJ7017 <aayanjaved79@gmail.com>
2024-08-08 22:12:37 +0500

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
 
 void main() {
   runApp(myapp());
@@ -9,12 +10,22 @@ class myapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+
+    //int day1=30;
+    //double days=30;
+    //String name="aj";
+    //bool exists=true;
+    //num temp=30.5; //can hold both int and double
+
+    //var daytoday="TUESDAY"; //compiler assigns data type
+    //const pi=3.14; //data cannot be modified
+    //final pi2=3.14; //like constant but data can be modified
+    //i.e we have a list if it is const it cannot be modified while vice versa if it is final
+
+
     return MaterialApp(
-      home: Material(
-        child:Center(
-          child: Container(
-            child: Text('Welcome to my app'),
-    ),
-    )));
+      home: HomePage(),
+      );
   }
 }
