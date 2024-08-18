a4cd02388e594df5cd7e65c46b42e4225b83d13d
AJ7017 <aayanjaved79@gmail.com>
2024-08-18 20:27:32 +0500

    day 13 completed

--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -38,13 +38,47 @@ class _HomePageState extends State<HomePage> {
         body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: (CatalogModel.Items !=null &&  CatalogModel.Items.isNotEmpty)? 
-          ListView.builder(
-            itemCount: CatalogModel.Items.length,
-            itemBuilder: (context, index) {
-              return ItemWidget(item: CatalogModel.Items[index], );
-            },
-          ):
-          Center(child: CircularProgressIndicator(),),
+          GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 16,
+              crossAxisSpacing: 16,
+              ),
+             itemBuilder: (context,index){
+              final item= CatalogModel.Items[index];
+              return Card(
+                clipBehavior: Clip.antiAlias,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                child: GridTile(
+                  header: Container(
+                    child: Text(item.name,style: TextStyle(color: Colors.white),),
+                    padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                    ),
+                    ),
+                  child: Image.network(item.image),
+                  footer: Container(
+                    child: Text(item.price.toString(),style: TextStyle(color: Colors.white),),
+                    padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                    ),
+                    ),
+                )
+              );
+             },
+              itemCount: CatalogModel.Items.length,
+              )
+          // ListView.builder(
+          //   itemCount: CatalogModel.Items.length,
+          //   itemBuilder: (context, index) {
+          //     return ItemWidget(item: CatalogModel.Items[index], );
+          //   },
+           
+          :Center(
+            child: CircularProgressIndicator(),
+            ),
         ),
           drawer: MyDrawer(),
         );
